429d433dc54bc5d029206e73732d326b4311c04b
Vincent Mahnke <vincent@mahn.ke>
2025-07-29 22:15:34 +0200

    docs: Fixes automated generation

--- example/allinkl/main.tf
+++ example/allinkl/main.tf
@@ -10,9 +10,9 @@ provider "allinkl" {
 }
 
 resource "allinkl_dns" "sub_test" {
-    record_name = "sub"
-    record_type = "A"
-    record_data = "1.2.3.4"
-    record_aux = "0"
-    zone_host = "vimaster.de"
+  record_name = "sub"
+  record_type = "A"
+  record_data = "1.2.3.4"
+  record_aux  = "0"
+  zone_host   = "vimaster.de"
 }
