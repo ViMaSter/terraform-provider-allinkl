51343085e0ed92322fd4c05dc7cc40e778ec0c0c
Vincent Mahnke <vincent@mahn.ke>
2025-11-28 21:48:35 +0000

    fix: Removes unused credentials

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -5,20 +5,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
 
-const (
-	// providerConfig is a shared configuration to combine with the actual
-	// test configuration so the AllInkl client is properly configured.
-	// It is also possible to use the ALLINKL_ environment variables instead,
-	// such as updating the Makefile and running the testing through that tool.
-	providerConfig = `
-provider "allinkl" {
-  kas_login = "w0117660"
-  kas_auth_type = "plain"
-  kas_auth_data = "s!Kj7MhCavM5a3KT=M7eRömWKuQdZf"
-}
-`
-)
-
 var (
 	// testAccProtoV6ProviderFactories are used to instantiate a provider during
 	// acceptance testing. The factory function will be invoked for every Terraform
