cef14d2aaa3a2e87cc64aa745d3ee7450b765d08
Vincent Mahnke <vincent@mahn.ke>
2025-12-05 02:34:19 +0100

    docs: Clarifies on DNS import

--- examples/resources/allinkl_dns/import.sh
+++ examples/resources/allinkl_dns/import.sh
@@ -1,2 +1,5 @@
 # dns entries are imported using their ID
+# when editing a DNS record via the KAS, the browser URL will follow
+# https://kas.all-inkl.com/index.php?...&id=$ID
+# use the value of $ID to import the DNS record:
 terraform import allinkl_dns.home 123456789
